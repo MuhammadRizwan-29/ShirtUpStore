ab5b97b843485ddbdf58ff9fb4c4183669155adf
Muhammad Rizwan <118816931+MuhammadRizwan-29@users.noreply.github.com>
2023-04-18 02:55:03 +0500

    Adding Design of Remaining other page to Main Repo

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,6 +1,9 @@
 import styled from "styled-components"
 import Annoucment from "../components/Annoucment"
 import Navbar from "../components/Navbar"
+import Products from "../components/Products"
+import NewsLetter from "../components/NewsLetter"
+import Footer from "../components/Footer"
 
 
 const Container = styled.div``
@@ -14,7 +17,18 @@ const FilterContainer = styled.div`
 const Filter = styled.div`
     margin: 20px;
 `
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+    margin-right: 20px;
+`
+const Select = styled.select`
+    padding: 10px;
+    margin-right: 20px;
+`
+const Option = styled.option`
 
+`
 export default function ProductList(){
     return(
         <Container>
@@ -22,8 +36,38 @@ export default function ProductList(){
             <Navbar />
             <Title>Dresses</Title>
             <FilterContainer>
-                <Filter></Filter>
+                <Filter>
+                    <FilterText>Filter Products: </FilterText>
+                    <Select>
+                        <Option disabled selected>Color</Option>
+                        <Option>White</Option>
+                        <Option>Black</Option>
+                        <Option>Yellow</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                        <Option>Green</Option>
+                    </Select>
+                    <Select>
+                        <Option disabled selected>Size</Option>
+                        <Option>XS</Option>
+                        <Option>S</Option>
+                        <Option>M</Option>
+                        <Option>L</Option>
+                        <Option>XL</Option>
+                    </Select>
+                </Filter>
+                <Filter>
+                    <FilterText>Sort Products:</FilterText>
+                    <Select>
+                        <Option selected>Newest</Option>
+                        <Option>Price (asen)</Option>
+                        <Option>Price (desen)</Option>
+                    </Select>
+                </Filter>
             </FilterContainer>
+            <Products />
+            <NewsLetter />
+            <Footer />
         </Container>
     )
 }
